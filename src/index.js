27457a5a86dc711edaa593e6b94b895de1746912
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-01 19:19:28 +0530

    Add logo image and update basic styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import logoimg from './media/FoodHoodieslogo.png';
+
+const LOGO = document.querySelector('.logo');
+const limg = new Image();
+limg.src = logoimg;
+limg.className = 'logo-img';
+limg.setAttribute('alt', 'FOOD-HOODIES logo');
+LOGO.appendChild(limg);
