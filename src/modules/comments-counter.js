a77ae57769835873b8dff555cf02dd1a27970a20
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-03 02:04:20 +0530

    Create comments counter method

---
+++ src/modules/comments-counter.js
@@ -0,0 +1,12 @@
+import { getComments } from "./comments-interaction.js";
+const countCommnets = async (id) => {
+    const temp = await getComments(id);
+    if (temp.length > 0) {
+        return temp.length;
+    }
+    else {
+        return 0;
+    }
+}
+
+export default countCommnets;
